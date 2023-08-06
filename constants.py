2d27042f62fac35ff7c670c4c47a961704ab3ca4
Vexus1 <adriangalik123@gmail.com>
2023-08-07 01:24:20 +0200

    updates

---
+++ constants.py
@@ -0,0 +1,6 @@
+WINDOW_WIDTH = 1440
+WINDOW_HEIGHT = 900
+FPS = 60
+
+SCENE_SPLASH = 'scene splash'
+SCENE_PLAY = 'scene play'
---
+++ scenes/scene_splash.py
@@ -0,0 +1,24 @@
+import pyghelpers
+import pygwidgets
+from constants import *
+
+class SceneSplash(pyghelpers.Scene):
+    def __init__(self, window):
+        self.window = window
+        self.start_button = pygwidgets.TextButton(window, (500, 100), 'Start')
+        self.quit_button = pygwidgets.TextButton(window, (500, 200), 'Quit')
+    
+    def getSceneKey(self):
+        return SCENE_SPLASH
+    
+    def handleInputs(self, events, keyPressedList):
+        for event in events:
+            if self.start_button.handleEvent(event):
+                self.goToScene(SCENE_PLAY)
+            elif self.quit_button.handleEvent(event):
+                self.quit()
+            print()
+    
+    def draw(self):
+        self.start_button.draw()
+        self.quit_button.draw()
