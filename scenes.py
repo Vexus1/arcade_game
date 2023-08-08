6c49c3967e18f490f688bc1a3ecfc3f55628859b
Vexus1 <adriangalik123@gmail.com>
2023-08-08 13:20:20 +0200

    minor updates

--- scenes.py
+++ scenes.py
@@ -1,50 +1,28 @@
 from abc import ABC, abstractmethod
 
-class scene(ABC):
+class SceneMenager()
+    def __init__(self):
+        pass
+
+
+class Scene(ABC):
     @abstractmethod
     def handle_inputs(self, events, keys_pressed):
-        """This method is called in every frame of the scene to handle events and key presses
-
-        Your code MUST override this method.
-
-        Parameters:
-            |    events - a list of events your method should handle.
-            |    keys_pressed - a list of keys that are pressed (a Boolean for each key).
-
-        """
         raise NotImplementedError
     
     def update(self):
-        """This method is called in every frame of the scene do any processing you need to do here
-
-        Your code will typically override this method.
-
-        """
         pass
 
     @abstractmethod
     def draw(self):
-        """This method is called in every frame of the scene to draw anything that needs to be drawn
-
-        Your code MUST override this method.
-
-        """
         raise NotImplementedError
     
     @abstractmethod
     def get_scene(self):
-        """This method must return the scene key for this scene
-
-        Your code MUST override this method.
-
-        """
         raise NotImplementedError
     
     def next_scene(self, next_scene):
-        """Call this method whenever you want to go to a new scene
-
-        Parameters:
-            |    nextSceneKey - the scene key (string) of the scene to go to
-
-        """
         return next_scene
+    
+    def _reference_to_scene_mgr(self, scene_mgr):
+        self.scene_mgr = scene_mgr
