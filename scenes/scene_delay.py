01228fffcd47919c84574b58f5f54ae9cccfb395
Vexus1 <adriangalik123@gmail.com>
2023-10-07 17:17:36 +0200

    minor changes

--- scenes/scene_delay.py
+++ scenes/scene_delay.py
@@ -3,7 +3,7 @@ import pygame
 class Delay:
     def scene_starting_delay(func):
         """Delay at the start of the level in milliseconds"""
-        def inner(self, *kwargs, **args):
+        def inner(self, *args, **kwargs):
             if pygame.time.get_ticks() >= self.scene_delay:
-                func(self, *kwargs, **args)
+                func(self, *args, **kwargs)
         return inner
